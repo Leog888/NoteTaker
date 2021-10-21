98b7f2c943ed3c74499cce3fce7f3c07f358c276
Leo <leonardodi.gutierrez@gmail.com>
2021-10-20 20:46:36 -0700

    changing the notetaker route

--- Route/notetaker.js
+++ Route/notetaker.js
@@ -1,15 +1,34 @@
-const notetaker = require('express').Router();
+const nt = require('express').Router();
 const { readAndAppend, readFromFile } = require('../Helper/fsUtilis');
 
 
 // Get Route for retrieving all the notetaker 
-notetaker.get('/', (req, res) =>{
+// nt = notetaker
+nt.get('/', (req, res) =>{
     readFromFile('./db/db.json')
     .then((data) => res.json(JSON.parse(data)))
 });
 
-notetaker.post('/', (req, res) =>{
-    const {  } = req.body;
+// Post for saving the notes
+nt.post('/', (req, res) =>{
 
-    
-})
+// 
+    const {title, text } = req.body;
+// If one of the the required properties are present
+    if(title || text) {
+        // Variable for the object will save
+        const newNotetaker = {
+            title,
+            text,
+        };
+        readAndAppend(newNotetaker, './db/db.json');
+
+        const response = {
+            status: 'Succes',
+            body: newNotetaker,
+        };
+
+        res.json(response);
+    }});
+
+    module.exports = nt;
