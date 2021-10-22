87b5b67486d252a22ad11a4882a6a0e839e616b7
Leo <leonardodi.gutierrez@gmail.com>
2021-10-21 23:03:05 -0700

    debugged

--- Develop/Route/index.js
+++
@@ -1,8 +0,0 @@
-const miniApp = require('express').Router();
-
-// Import the routers for notetaker
-const notetakerRouter = require('./notetaker');
-
-miniApp.use('/nt', notetakerRouter);
-
-module.exports = miniApp;
--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Route/index');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+const { readFromFile, readAndAppend } = require('./Helper/fsUtilis');
+const { v4:uuid } = require('uuid');
 
 
 // Middleware to parse the data and urlencoded from data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -23,6 +23,36 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
-app.listen(PORT, () =>
+
+// Get Route for retrieving all the notetaker 
+app.get('/api/notes', (req, res) =>{
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)))
+});
+
+// Post for saving the notes
+app.post('/api/notes', (req, res) =>{
+
+// 
+    const { title, text } = req.body;
+// If one of the the required properties are present
+    if( title || text || id) {
+        // Variable for the object will save
+        const newNotetaker = {
+            title: title,
+            text: text,
+            id: uuid(),
+        };
+        readAndAppend(newNotetaker, './db/db.json');
+
+        const response = {
+            status: 'Succes',
+            body: newNotetaker,
+        };
+
+        res.json(response);
+    }});
+
+    app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
 );
